d82b6f451e4cb985107a97adac82c1ea41245ef6
zevinal <ariankrasniqi23@gmail.com>
2024-03-25 01:27:42 +0000

    Added walking animations

--- GAME/js/game.js
+++ GAME/js/game.js
@@ -56,7 +56,7 @@ var WorldScene = new Phaser.Class({
         obstacles.setCollisionByExclusion([-1]);
 
 		// Create player character
-		this.player = this.physics.add.sprite(50, 100, 'player', 6)
+		this.player = this.physics.add.sprite(50, 100, 'player', 0)
 
 		// Apply physics boundaries
 		this.physics.world.bounds.width = map.widthInPixels;
@@ -70,6 +70,35 @@ var WorldScene = new Phaser.Class({
 		this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
 		this.cameras.main.startFollow(this.player);
 		this.cameras.main.roundPixels = true;
+
+		// Player character animations
+		this.anims.create({
+			key: 'left',
+			frames: this.anims.generateFrameNumbers('player', { frames: [1, 7, 1, 13]}),
+			frameRate: 10,
+			repeat: -1
+		});
+
+		this.anims.create({
+			key: 'right',
+			frames: this.anims.generateFrameNumbers('player', { frames: [1, 7, 1, 13]}),
+			frameRate: 10,
+			repeat: -1
+		});
+
+		this.anims.create({
+			key: 'up',
+			frames: this.anims.generateFrameNumbers('player', { frames: [2, 8, 2, 14]}),
+			frameRate: 10,
+			repeat: -1
+		});
+
+		this.anims.create({
+			key: 'down',
+			frames: this.anims.generateFrameNumbers('player', { frames: [0, 6, 0, 12]}),
+			frameRate: 10,
+			repeat: -1
+		});
 	},
 
 	update: function (time, delta)
@@ -80,20 +109,29 @@ var WorldScene = new Phaser.Class({
 		if (this.cursors.left.isDown)
 		{
 			this.player.body.setVelocityX(-80);
+			this.player.anims.play('left', true);
+			this.player.flipX=true;
 		}
 		else if (this.cursors.right.isDown)
 		{
 			this.player.body.setVelocityX(80);
+			this.player.anims.play('right', true);
+			this.player.flipX=false;
 		}
 
 		// Vertical movement
-		if (this.cursors.up.isDown)
+		else if (this.cursors.up.isDown)
 		{
 			this.player.body.setVelocityY(-80);
+			this.player.anims.play('up', true);
 		}
 		else if (this.cursors.down.isDown)
 		{
 			this.player.body.setVelocityY(80);
+			this.player.anims.play('down', true);
+		}
+		else {
+			this.player.anims.stop();
 		}
 	}
 });
